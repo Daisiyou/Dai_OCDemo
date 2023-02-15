56ab8651d6949190eb533d88fde8e89f4a990e2b
Dai <490903692@qq.com>
2023-02-15 20:58:25 +0800

    init project

---
+++ Dai_OCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dai_OCDemo
+//
+//  Created by Dai on 2023/2/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
